bd704deb0e9de565ec2e511ebfa3b8797fa4dcbd
TienDat <trtiendat.004@@gmail.com>
2026-05-30 17:23:14 +0700

    COMPLETE EX_02 SES_04

---
+++ session_04_portfolio_react/src/components/Portfolio.jsx
@@ -0,0 +1,12 @@
+function Portfolio() {
+    return (
+        <section id="portfolio">
+            <div className="container">
+                <h2>My Portfolio</h2>
+                <p>Projects coming soon...</p>
+            </div>
+        </section>
+    );
+}
+
+export default Portfolio;
